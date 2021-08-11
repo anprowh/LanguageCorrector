4a12a28cf1601067c6db54e14b468b00b6086eef
Random-is <randomisbox@gmail.com>
2021-08-11 21:41:15 +1000

    fix code formatting

--- selected_text_changer.py
+++ selected_text_changer.py
@@ -1,12 +1,12 @@
 from enum import Enum
-import pyperclip
-from typing import List, Optional, Dict, Tuple
-from sklearn.preprocessing import OneHotEncoder
-import tensorflow as tf
-import numpy as np
-import keyboard
 from time import sleep
-import sys
+from typing import List, Optional
+
+import keyboard
+import numpy as np
+import pyperclip
+import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
 
 
 class Lang(str, Enum):
@@ -39,17 +39,18 @@ class WordClassifier:
         self.rus_letters = 'йцукенгшщзхъфывапролджэячсмитьбюё'
         eng_letters = 'qwertyuiop[]asdfghjkl;\'zxcvbnm,.`'
         self.eng_to_rus_dict = {e: r for e,
-                                r in zip(eng_letters, self.rus_letters)}
+                                         r in zip(eng_letters, self.rus_letters)}
         self.model = tf.keras.models.load_model('./word_classifier.hdf5')
         self.char_encoder = OneHotEncoder()
         self.char_encoder.fit([[c] for c in self.rus_letters])
 
     def classify_words(self, words: List[str]):
-        return np.argmax(self.model.predict(self.transform_words(words).reshape((-1, 33*self.n_letters))), axis=-1)
+        return np.argmax(self.model.predict(self.transform_words(words).reshape((-1, 33 * self.n_letters))), axis=-1)
 
     def transform_word(self, word: str) -> np.ndarray:
         word = np.array(list(word)).reshape((-1, 1))
-        return np.append(self.char_encoder.transform(word).toarray(), (np.zeros((self.n_letters, 33))), axis=0)[:self.n_letters]
+        return np.append(self.char_encoder.transform(word).toarray(), (np.zeros((self.n_letters, 33))), axis=0)[
+               :self.n_letters]
 
     def transform_words(self, words: List[str]) -> np.ndarray:
         return np.array([self.transform_word(x) for x in words])
@@ -76,7 +77,7 @@ def change_language(classifier: WordClassifier):
 
     ext_rus_letters = rus_letters + rus_letters.upper() + '.,"№;:?/'
     ext_eng_letters = eng_letters + \
-        'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>~/?@#$^&|'
+                      'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>~/?@#$^&|'
     rus_to_eng_dict = {r: e for r, e in zip(ext_rus_letters, ext_eng_letters)}
     eng_to_rus_dict = {e: r for r, e in zip(ext_rus_letters, ext_eng_letters)}
 
@@ -89,9 +90,8 @@ def change_language(classifier: WordClassifier):
     sleep(0.1)
     keyboard.send('ctrl+v')
 
-
-if __name__ == '__main__':
-    classifier = WordClassifier()
-    hotkey = open('config.txt', 'r').read().strip().split(':')[1].strip()
-    keyboard.add_hotkey(hotkey, change_language, args=(classifier,))
-    keyboard.wait('esc')
+# if __name__ == '__main__':
+#     classifier = WordClassifier()
+#     hotkey = open('config.txt', 'r').read().strip().split(':')[1].strip()
+#     keyboard.add_hotkey(hotkey, change_language, args=(classifier,))
+#     keyboard.wait('esc')
